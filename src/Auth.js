1a2ef276c551b20707c7d17bd77f7d7f8c90ab01
Damian Ovidiu-Cristian <damian.ovidiu27@yahoo.com>
2022-06-09 00:47:13 +0300

    Undone prettier damage

--- src/Auth.js
+++ src/Auth.js
@@ -1,44 +1,16 @@
 import { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
-import {
-  Button,
-  Stack,
-  Alert,
-  AlertIcon,
-  Box,
-  AlertTitle,
-  AlertDescription,
-  Text,
-  Image,
-  Link,
-  Wrap,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Button, Stack, Alert, AlertIcon, Box, AlertTitle, AlertDescription, Text, Image, Link, Wrap, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import walletConnect from "./img/wallet-connect.png";
 import walletMetamask from "./img/wallet-metamask.png";
 import chainElrond from "./img/elrond-chain-logo.png";
 import { DappUI } from "@elrondnetwork/dapp-core";
-import {
-  useGetAccountInfo,
-  refreshAccount,
-  sendTransactions,
-} from "@elrondnetwork/dapp-core";
+import { useGetAccountInfo, refreshAccount, sendTransactions } from "@elrondnetwork/dapp-core";
 
 export const Auth = () => {
-  const {
-    ExtensionLoginButton,
-    WebWalletLoginButton,
-    LedgerLoginButton,
-    WalletConnectLoginButton,
-  } = DappUI;
-  
+  const { ExtensionLoginButton, WebWalletLoginButton, LedgerLoginButton, WalletConnectLoginButton } = DappUI;
+
   const { address: elrondAddress } = useGetAccountInfo();
 
   const WALLETS = {
@@ -47,18 +19,8 @@ export const Auth = () => {
     ELROND: 3,
   };
 
-  const {
-    authenticate,
-    isAuthenticating,
-    Moralis,
-    authError,
-    isAuthenticated,
-  } = useMoralis();
-  const {
-    isOpen: isProgressModalOpen,
-    onOpen: onProgressModalOpen,
-    onClose: onProgressModalClose,
-  } = useDisclosure();
+  const { authenticate, isAuthenticating, Moralis, authError, isAuthenticated } = useMoralis();
+  const { isOpen: isProgressModalOpen, onOpen: onProgressModalOpen, onClose: onProgressModalClose } = useDisclosure();
 
   const [authErrorUi, setAuthErrorUi] = useState(null);
   const [walletUsed, setWalletUsed] = useState(WALLETS.METAMASK);
@@ -147,31 +109,21 @@ export const Auth = () => {
         Connect a Wallet
       </Button>
 
-      <Modal
-        size="xl"
-        isOpen={isProgressModalOpen}
-        onClose={handleProgressModalClose}
-        closeOnEsc={false}
-        closeOnOverlayClick={false}
-      >
+      <Modal size="xl" isOpen={isProgressModalOpen} onClose={handleProgressModalClose} closeOnEsc={false} closeOnOverlayClick={false}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Select a Wallet</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
             <Stack spacing="5">
-              <Text fontSize="sm">
-                Please select a wallet to connect to the Data DEX
-              </Text>
+              <Text fontSize="sm">Please select a wallet to connect to the Data DEX</Text>
 
               {authErrorUi && (
                 <Alert status="error">
                   <AlertIcon />
                   <Box flex="1">
                     <AlertTitle>Authentication has failed</AlertTitle>
-                    <AlertDescription display="block">
-                      {authErrorUi.message}
-                    </AlertDescription>
+                    <AlertDescription display="block">{authErrorUi.message}</AlertDescription>
                   </Box>
                 </Alert>
               )}
@@ -179,72 +131,31 @@ export const Auth = () => {
               <Box p="15px">
                 <Wrap spacing="20px" justify="space-between">
                   <Box>
-                    <Button
-                      isLoading={Boolean(isAuthenticatingMetamask)}
-                      onClick={() => handleAuthenticate()}
-                      width="220px"
-                      p="8"
-                    >
-                      <Image
-                        src={walletMetamask}
-                        boxSize="40px"
-                        borderRadius="lg"
-                        mr="2"
-                      />
+                    <Button isLoading={Boolean(isAuthenticatingMetamask)} onClick={() => handleAuthenticate()} width="220px" p="8">
+                      <Image src={walletMetamask} boxSize="40px" borderRadius="lg" mr="2" />
                       <Text>MetaMask</Text>
                     </Button>
                   </Box>
 
                   <Box>
-                    <Button
-                      isDisabled={true}
-                      isLoading={Boolean(isAuthenticatingWc)}
-                      onClick={() => handleAuthenticate(WALLETS.WC)}
-                      width="220px"
-                      p="8"
-                    >
-                      <Image
-                        src={walletConnect}
-                        boxSize="40px"
-                        borderRadius="lg"
-                        mr="2"
-                      />
+                    <Button isDisabled={true} isLoading={Boolean(isAuthenticatingWc)} onClick={() => handleAuthenticate(WALLETS.WC)} width="220px" p="8">
+                      <Image src={walletConnect} boxSize="40px" borderRadius="lg" mr="2" />
                       WalletConnect
                     </Button>
                   </Box>
 
                   <Box>
-                    <Button
-                      isLoading={Boolean(isAuthenticatingElrond)}
-                      onClick={() => handleAuthenticate(WALLETS.ELROND)}
-                      width="220px"
-                      p="8"
-                    >
-                      <Image
-                        src={chainElrond}
-                        boxSize="40px"
-                        borderRadius="lg"
-                        mr="2"
-                      />
+                    <Button isLoading={Boolean(isAuthenticatingElrond)} onClick={() => handleAuthenticate(WALLETS.ELROND)} width="220px" p="8">
+                      <Image src={chainElrond} boxSize="40px" borderRadius="lg" mr="2" />
                       Elrond via Ledger
                     </Button>
                   </Box>
                   <Box>
-                    <Button
-                      isLoading={Boolean(isAuthenticatingElrond)}
-                    >
-                      <ExtensionLoginButton
-                        callbackRoute={"/"}
-                        loginButtonText={"Extension"}
-                      ></ExtensionLoginButton>
+                    <Button isLoading={Boolean(isAuthenticatingElrond)}>
+                      <ExtensionLoginButton callbackRoute={"/"} loginButtonText={"Extension"}></ExtensionLoginButton>
                     </Button>
-                    <Button
-                      isLoading={Boolean(isAuthenticatingElrond)}
-                    >
-                      <WalletConnectLoginButton
-                        callbackRoute={"/"}
-                        loginButtonText={"Maiar"}
-                      ></WalletConnectLoginButton>
+                    <Button isLoading={Boolean(isAuthenticatingElrond)}>
+                      <WalletConnectLoginButton callbackRoute={"/"} loginButtonText={"Maiar"}></WalletConnectLoginButton>
                     </Button>
                   </Box>
                 </Wrap>
