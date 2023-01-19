f5aa3bfebfa409b5e768ad3e797792247790e35c
icegriffinguru <icegriffin1122@gmail.com>
2023-01-19 13:30:57 -0500

    [Ice] marketplace update 1

--- src/DataNFT/DataNFTMarketplaceMultiversX.tsx
+++ src/DataNFT/DataNFTMarketplaceMultiversX.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Box, Stack } from '@chakra-ui/layout';
 import {
   Skeleton, CloseButton, HStack, Badge, ButtonGroup, Button,
-  Alert, AlertIcon, AlertTitle, Heading, Image, Flex, Link, Text, Tooltip, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper
+  Alert, AlertIcon, AlertTitle, Heading, Image, Flex, Link, Text, Tooltip, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverArrow, PopoverCloseButton, PopoverBody, useDisclosure, ModalOverlay, ModalContent, Modal, ModalHeader, ModalBody
 } from '@chakra-ui/react';
 import SkeletonLoadingList from 'UtilComps/SkeletonLoadingList';
 import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks/account';
@@ -18,11 +18,15 @@ export default function Marketplace() {
   const { address } = useGetAccountInfo();
   const { hasPendingTransactions } = useGetPendingTransactions();
   const isLoggedIn = Boolean(address);
+  const [tabState, setTabState] = useState<number>(1);  // 1 for "Public Marketplace", 2 for "My Data NFTs"
   const [tokensForSale, setTokensForSale] = useState<any[]>([]);
   const [amountOfTokens, setAmountOfTokens] = useState<any>({});
   const [numberOfPages, setNumberOfPages] = useState<number>(1);
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [selectedItem, setSelectedItem] = useState<any>({});
   const contract = new DataNftMarketContract('ED');
+  
+  const { isOpen: isProcureModalOpen, onOpen: onProcureModalOpen, onClose: onProcureModalClose } = useDisclosure();
 
   useEffect(() => {
     contract.getNumberOfOffers().then((nr:any) => {
@@ -57,25 +61,47 @@ export default function Marketplace() {
   }, [address, hasPendingTransactions]);
 
   return (
-    <Stack spacing={5}>
-      <Heading size="lg">Data NFT Marketplace</Heading>
-      <Heading size="xs" opacity=".7">Browse, View and Buy Cross-Chain Data NFTs</Heading>
+    <>
+      <Stack spacing={5}>
+        <Heading size="lg">Data NFT Marketplace</Heading>
+        <Flex
+          mt="5"
+          gap='12px'
+          justifyContent={{ base: 'center', md: 'flex-start' }}
+          flexDirection={{ base: 'row', md: 'row' }}
+        >
+          <Button
+            colorScheme="teal"
+            width={{ base: '160px', md: '160px' }}
+            disabled={tabState === 1}
+            onClick={() => setTabState(1)}
+          >
+            Public Marketplace
+          </Button>
+          <Button
+            colorScheme="teal"
+            width={{ base: '160px', md: '160px' }}
+            disabled={tabState === 2}
+            onClick={() => setTabState(2)}
+          >
+            My Data NFTs
+          </Button>
+        </Flex>
 
-      {(!tokensForSale || tokensForSale && tokensForSale.length === 0) &&
-        <>{!noData && <SkeletonLoadingList /> || <Text>No data yet...</Text>}</> ||
-        <Flex wrap="wrap">
+        {(!tokensForSale || tokensForSale && tokensForSale.length === 0) &&
+          <>{!noData && <SkeletonLoadingList /> || <Text>No data yet...</Text>}</> ||
+          <Flex wrap="wrap">
 
-          {tokensForSale && tokensForSale.map((token) => <Box key={token.index} maxW="xs" borderWidth="1px" borderRadius="lg" overflow="wrap" mr="1rem" w="250px" mb="1rem" position="relative">
-            <Flex justifyContent="center" pt={5}>
-              <Skeleton isLoaded={oneNFTImgLoaded} h={200}>
-                <Image
-                  src={`https://${getApi('ED')}/nfts/${token['have']['identifier']}-${hexZero(token['have']['nonce'])}/thumbnail`}
-                  alt={'item.dataPreview'} h={200} w={200} borderRadius="md" onLoad={() => setOneNFTImgLoaded(true)} />
-              </Skeleton>
-            </Flex>
+            {tokensForSale && tokensForSale.map((token) => <Box key={token.index} maxW="xs" borderWidth="1px" borderRadius="lg" overflow="wrap" mr="1rem" w="250px" mb="1rem" position="relative">
+              <Flex justifyContent="center" pt={5}>
+                <Skeleton isLoaded={oneNFTImgLoaded} h={200}>
+                  <Image
+                    src={`https://${getApi('ED')}/nfts/${token['have']['identifier']}-${hexZero(token['have']['nonce'])}/thumbnail`}
+                    alt={'item.dataPreview'} h={200} w={200} borderRadius="md" onLoad={() => setOneNFTImgLoaded(true)} />
+                </Skeleton>
+              </Flex>
 
-            <Flex p="3" direction="column" justify="space-between" height="150px">
-              <Box>
+              <Flex p="3" direction="column" justify="flex-start" height="150px">
                 <Box as="span" fontSize="sm">
                   {(
                     token['have']['amount'] /
@@ -92,103 +118,144 @@ export default function Marketplace() {
                   <Text>Supply: {token['quantity']}</Text>
                 </Box>
 
-                <Box as="span" fontSize="sm">
-                  <Text>
-                    Price:
-                    {' ' +
-                      token['want']['amount'] /
-                      Math.pow(10, tokenDecimals(token['want']['identifier'])) +
-                      ' '}
-                    {getTokenWantedRepresentation(
-                      token['want']['identifier'],
-                      token['want']['nonce']
-                    )}
-                  </Text>
-                </Box>
-              </Box>
-              <HStack>       
-                <Text fontSize='sm'>Amount to buy: </Text>
-                <NumberInput size="xs" maxW={16} step={1} defaultValue={1} min={1} max={token['quantity']} value={amountOfTokens[token.index]} onChange={(valueString) => setAmountOfTokens((oldAmounts:any)=>{
-                  const newAmounts = { ...oldAmounts };
-                  newAmounts[token.index] = Number(valueString);
-                  return newAmounts;
-                })}>
-                  <NumberInputField />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-                </HStack>
+                {/* Hide Procure part if NFT is owned by User */}
+                {
+                  address && address == token.owner && (<>
+                    <Box as="span" fontSize="sm">
+                      <Text>
+                        Fee per NFT:
+                        {' ' +
+                          token['want']['amount'] * amountOfTokens[token.index] /
+                          Math.pow(10, tokenDecimals(token['want']['identifier'])) +
+                          ' '}
+                        {getTokenWantedRepresentation(
+                          token['want']['identifier'],
+                          token['want']['nonce']
+                        )}
+                      </Text>
+                    </Box>
+                    
+                    <HStack>       
+                      <Text fontSize='sm'>How many to procure access to </Text>
+                      <NumberInput size="xs" maxW={16} step={1} defaultValue={1} min={1} max={token['quantity']} value={amountOfTokens[token.index]} onChange={(valueString) => setAmountOfTokens((oldAmounts:any)=>{
+                        const newAmounts = { ...oldAmounts };
+                        newAmounts[token.index] = Number(valueString);
+                        return newAmounts;
+                      })}>
+                        <NumberInputField />
+                        <NumberInputStepper>
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
+                        </NumberInputStepper>
+                      </NumberInput>
+                      <Button
+                        size="xs"
+                        colorScheme="teal"
+                        width="72px"
+                        onClick={() => {
+                          setSelectedItem(token);
+                          onProcureModalOpen();
+                        }}
+                      >
+                        Procure
+                      </Button>
+                    </HStack>
+                  </>)
+                }
 
-              <Button size="xs" mt={3} colorScheme="teal" variant="outline" onClick={() => {
-                if (token['want']['identifier'] === 'EGLD') {
-                  contract.sendAcceptOfferEgldTransaction(
-                    token['index'],
-                    token['want']['amount'],
-                    amountOfTokens[token['index']],
-                    address
-                  );
-                } else {
-                  if (token['want']['nonce'] === 0) {
-                    contract.sendAcceptOfferEsdtTransaction(
+                {/* <Button size="xs" mt={3} colorScheme="teal" variant="outline" onClick={() => {
+                  if (token['want']['identifier'] === 'EGLD') {
+                    contract.sendAcceptOfferEgldTransaction(
                       token['index'],
                       token['want']['amount'],
-                      token['want']['identifier'],
                       amountOfTokens[token['index']],
                       address
                     );
                   } else {
-                    contract.sendAcceptOfferNftEsdtTransaction(
-                      token['index'],
-                      token['want']['amount'],
-                      token['want']['identifier'],
-                      token['want']['nonce'],
-                      amountOfTokens[token['index']],
-                      address
-                    );
+                    if (token['want']['nonce'] === 0) {
+                      contract.sendAcceptOfferEsdtTransaction(
+                        token['index'],
+                        token['want']['amount'],
+                        token['want']['identifier'],
+                        amountOfTokens[token['index']],
+                        address
+                      );
+                    } else {
+                      contract.sendAcceptOfferNftEsdtTransaction(
+                        token['index'],
+                        token['want']['amount'],
+                        token['want']['identifier'],
+                        token['want']['nonce'],
+                        amountOfTokens[token['index']],
+                        address
+                      );
+                    }
                   }
-                }
-              }}>
-                Buy {amountOfTokens[token['index']]} NFT{amountOfTokens[token['index']]>1&&'s'} for {(token['want']['amount'] *
-                  amountOfTokens[token['index']]) /
-                  Math.pow(
-                    10,
-                    tokenDecimals(token['want']['identifier'])
-                  ) +
-                  ' '}
-              </Button>
-            </Flex>
+                }}>
+                  Buy {amountOfTokens[token['index']]} NFT{amountOfTokens[token['index']]>1&&'s'} for {(token['want']['amount'] *
+                    amountOfTokens[token['index']]) /
+                    Math.pow(
+                      10,
+                      tokenDecimals(token['want']['identifier'])
+                    ) +
+                    ' '}
+                </Button> */}
+              </Flex>
 
-            <Box
-              position='absolute'
-              top='0'
-              bottom='0'
-              left='0'
-              right='0'
-              height='100%'
-              width='100%'
-              backgroundColor='blackAlpha.800'
-              visibility={userData.addressFrozen || (userData.frozenNonces && userData.frozenNonces.includes(token.have.nonce)) ? 'visible' : 'collapse'}
-            >
-              <Text
+              <Box
                 position='absolute'
-                top='50%'
-                // left='50%'
-                // transform='translate(-50%, -50%)'
-                textAlign='center'
-                fontSize='md'
-                px='2'
+                top='0'
+                bottom='0'
+                left='0'
+                right='0'
+                height='100%'
+                width='100%'
+                backgroundColor='blackAlpha.800'
+                visibility={userData.addressFrozen || (userData.frozenNonces && userData.frozenNonces.includes(token.have.nonce)) ? 'visible' : 'collapse'}
               >
-                - FROZEN - <br />
-                Data NFT is under investigation by the DAO as there was a complaint received against it
-              </Text>
-            </Box>
-          </Box>)}
+                <Text
+                  position='absolute'
+                  top='50%'
+                  // left='50%'
+                  // transform='translate(-50%, -50%)'
+                  textAlign='center'
+                  fontSize='md'
+                  px='2'
+                >
+                  - FROZEN - <br />
+                  Data NFT is under investigation by the DAO as there was a complaint received against it
+                </Text>
+              </Box>
+            </Box>)}
+          </Flex>
+        }
+      </Stack>
 
+      {
+        selectedItem && <Modal
+          isOpen={isProcureModalOpen}
+          onClose={onProcureModalClose}
+          closeOnEsc={false} closeOnOverlayClick={false}
+        >
+          <ModalOverlay
+            bg='blackAlpha.700'
+            backdropFilter='blur(10px) hue-rotate(90deg)'
+          />
+          <ModalContent>
+            <ModalBody py={6}>
+              <Text fontSize='md' mt='4'>How many : 6</Text>
+              <Text fontSize='md' mt='4'>Fee per NFT : 60 ITHEUM</Text>
+              <Text fontSize='md' mt='4'>Buyer Fee : 2%</Text>
+              <Text fontSize='md' mt='4'>Total Fee : 61.2 ITHEUM</Text>
 
-        </Flex>
+              <Flex justifyContent='end' mt='8 !important'>
+                <Button colorScheme="teal" size='sm' mx='3'>Proceed</Button>
+                <Button colorScheme="teal" size='sm' variant='outline' onClick={onProcureModalClose}>Cancel</Button>
+              </Flex>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
       }
-    </Stack>
+    </>
   );
 };
