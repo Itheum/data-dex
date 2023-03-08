5fc18d9ebb218581a44982790c1f1c51a96c9050
icegriffinguru <icegriffin1122@gmail.com>
2023-03-08 03:31:24 -0500

    [Ice] fix #322 (2)

--- src/AdvertiseData/SellDataMultiversX.tsx
+++ src/AdvertiseData/SellDataMultiversX.tsx
@@ -816,8 +816,7 @@ export default function SellDataMX({ onRfMount, itheumAccount }: { onRfMount: an
                 !!dataNFTMarshalServiceStatus ||
                 !dataNFTImgGenServiceValid ||
                 (!!userData && userData.contractWhitelistEnabled && !userData.userWhitelistedForMint) ||
-                (!!userData && userData.contractPaused)) ||
-                (!!userData && Date.now() < userData.lastUserMintTime + userData.mintTimeLimit)
+                (!!userData && userData.contractPaused))
                 && (
                   <Alert status="error">
                     <Stack>
@@ -838,7 +837,23 @@ export default function SellDataMX({ onRfMount, itheumAccount }: { onRfMount: an
                           <AlertDescription fontSize="md">You are not currently whitelisted to mint Data NFTs</AlertDescription>
                         )}
                         {!!userData && userData.contractPaused && <Text fontSize="md">The minter smart contract is paused for maintenance.</Text>}
-                        {!!userData && Date.now() < userData.lastUserMintTime + userData.mintTimeLimit && <Text fontSize="md">{`You can mint next Data NFT-FT after ${new Date(userData.lastUserMintTime + userData.mintTimeLimit).toLocaleString()}`}</Text>}
+                      </AlertDescription>
+                    </Stack>
+                  </Alert>
+                )}
+
+              {(!!userData && Date.now() < userData.lastUserMintTime + userData.mintTimeLimit)
+                && (
+                  <Alert status="error">
+                    <Stack>
+                      <AlertTitle fontSize="md" mb={2}>
+                        <AlertIcon display="inline-block" />
+                        <Text display="inline-block" lineHeight="2" style={{ verticalAlign: "middle" }}>
+                          Alerts
+                        </Text>
+                      </AlertTitle>
+                      <AlertDescription>
+                        {!!userData && Date.now() < userData.lastUserMintTime + userData.mintTimeLimit && <Text fontSize="md">{`There is a time interval enforced between mints. You can mint your next Data NFT-FT after ${new Date(userData.lastUserMintTime + userData.mintTimeLimit).toLocaleString()}`}</Text>}
                       </AlertDescription>
                     </Stack>
                   </Alert>
