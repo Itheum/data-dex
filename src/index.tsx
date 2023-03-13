e700cc04045ec745ea769a78bc13ad97015e45ae
Mark Paul <paul.mark@gmail.com>
2023-03-13 11:02:18 +1100

    adding sentry replays to test

--- src/index.tsx
+++ src/index.tsx
@@ -14,10 +14,18 @@ import "../src/MultiversX/custom.css";
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: process.env.REACT_APP_ENV_SENTRY_DSN,
-    integrations: [new BrowserTracing()],
 
-    // Set tracesSampleRate to 1.0 to capture 100%  of transactions for performance monitoring.
+    // (BrowserTracing) Set tracesSampleRate to 1.0 to capture 100%  of transactions for performance monitoring.
     tracesSampleRate: 1.0,
+
+    // (Replay) This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 1.0,
+    // (Replay) If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
+
+    integrations: [new BrowserTracing(), new Sentry.Replay()],
   });
 }
 
