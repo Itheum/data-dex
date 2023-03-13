25ae3bd3fa1e1f4f85712a97de40b3e31778ea1c
Mark Paul <paul.mark@gmail.com>
2023-03-13 11:44:17 +1100

    adding sentry custom envs for better filters

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,9 @@ if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: process.env.REACT_APP_ENV_SENTRY_DSN,
 
+    // this is so we can use the environments filter in sentry to filter staging production vs actual production
+    environment: process.env.REACT_APP_ENV_SENTRY_PROFILE || 'unknown',
+
     // (BrowserTracing) Set tracesSampleRate to 1.0 to capture 100%  of transactions for performance monitoring.
     tracesSampleRate: 1.0,
 
