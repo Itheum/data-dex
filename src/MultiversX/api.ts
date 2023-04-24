0ae9e70492261aa8ac73ff291399cc85434ad921
Damian <damian.ovidiu27@gmail.com>
2023-04-25 00:53:47 +0300

    #552

--- src/MultiversX/api.ts
+++ src/MultiversX/api.ts
@@ -1,32 +1,29 @@
 import { NftType, TokenType } from "@multiversx/sdk-dapp/types/tokens.types";
-import { ProxyNetworkProvider } from "@multiversx/sdk-network-providers/out";
+import { ApiNetworkProvider, ProxyNetworkProvider } from "@multiversx/sdk-network-providers/out";
 import axios from "axios";
 
 import { uxConfig } from "libs/util";
 
 export const getApi = (networkId: string) => {
-  if (networkId === "E1") {
-    return "api.multiversx.com";
-  } else {
-    return "devnet-api.multiversx.com";
-    return "elrond-api-devnet.blastapi.io/0bc98858-cb7a-44c6-ad1b-8c8bfaec7128";
-  }
+  const envKey = networkId === "E1" ? "REACT_APP_ENV_API_MAINNET_KEY" : "REACT_APP_ENV_API_DEVNET_KEY";
+  const defaultUrl = networkId === "E1" ? "api.multiversx.com" : "devnet-api.multiversx.com";
+
+  return process.env[envKey] || defaultUrl;
 };
 
-export const getGateway = (networkId: string) => {
-  if (networkId === "E1") {
-    return new ProxyNetworkProvider("https://gateway.multiversx.com");
-  } else {
-    return new ProxyNetworkProvider("https://devnet-gateway.multiversx.com");
-  }
+export const getNetworkProvider = (networkId?: string, chainId?: string) => {
+  const environment = networkId === "E1" || chainId === "1" ? "mainnet" : "devnet";
+  const envKey = environment === "mainnet" ? "REACT_APP_ENV_GATEWAY_MAINNET_KEY" : "REACT_APP_ENV_GATEWAY_DEVNET_KEY";
+  const defaultUrl = environment === "mainnet" ? "https://gateway.multiversx.com" : "https://devnet-gateway.multiversx.com";
+
+  const envValue = process.env[envKey];
+  const isApi = envValue && envValue.includes("api");
+
+  return isApi ? new ApiNetworkProvider(envValue) : new ProxyNetworkProvider(envValue || defaultUrl);
 };
 
 export const getExplorer = (networkId: string) => {
-  if (networkId === "E1") {
-    return "explorer.multiversx.com";
-  } else {
-    return "devnet-explorer.multiversx.com";
-  }
+  return networkId === "E1" ? "explorer.multiversx.com" : "devnet-explorer.multiversx.com";
 };
 
 export const getTransactionLink = (networkId: string, txHash: string) => {
