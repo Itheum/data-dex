835d353cb63e671102ddc1b15f5a6d1ebf834bfb
icegriffinguru <icegriffin1122@gmail.com>
2023-05-23 11:21:07 -0400

    [Ice] fix #661 ?

--- src/components/UtilComps/ShortAddress.tsx
+++ src/components/UtilComps/ShortAddress.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Text, Tooltip } from "@chakra-ui/react";
+import { Text, Tooltip, useToast } from "@chakra-ui/react";
 
 export default function ShortAddress({ address, fontSize = "xs", marginLeftSet = "auto" }: { address?: string; fontSize?: string; marginLeftSet?: string }) {
+  const toast = useToast();
   const internalAddress = address || "";
   return (
     <Tooltip label="click to copy" aria-label="click to copy" fontSize={fontSize}>
@@ -11,6 +12,12 @@ export default function ShortAddress({ address, fontSize = "xs", marginLeftSet =
         fontSize={fontSize}
         onClick={() => {
           navigator.clipboard.writeText(internalAddress);
+
+          toast({
+            title: "Address is copied to clipboard",
+            status: "success",
+            isClosable: true,
+          });
         }}
         style={{ cursor: "pointer" }}>
         {`${internalAddress.substring(0, 6)}...${internalAddress.slice(-4)}`}
