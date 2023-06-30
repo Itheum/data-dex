b314ad8304f4ca0e731c519065542e039ef0c4b5
Mark Paul <paul.mark@gmail.com>
2023-07-01 00:46:22 +1000

    new privacy policy link

--- src/components/Sections/AppFooter.tsx
+++ src/components/Sections/AppFooter.tsx
@@ -35,7 +35,7 @@ export default function () {
           <Link fontSize="xs" href="https://itheum.com/legal/datadex/termsofuse" isExternal>
             Terms of Use <ExternalLinkIcon mx={1} />
           </Link>
-          <Link fontSize="xs" href="https://itheum.com/privacypolicy" isExternal>
+          <Link fontSize="xs" href="https://itheum.com/legal/datadex/privacypolicy" isExternal>
             Privacy Policy <ExternalLinkIcon mx={1} />
           </Link>
         </HStack>
