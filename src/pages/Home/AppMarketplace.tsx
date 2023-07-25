ee32a64fabbd947028166efeb078f720ddfb553f
Bucur David <dbucur22@gmail.com>
2023-07-25 21:39:34 +0300

    Fixed #831

--- src/pages/Home/AppMarketplace.tsx
+++ src/pages/Home/AppMarketplace.tsx
@@ -39,9 +39,10 @@ import imgProgRhc from "assets/img/prog-rhc.png";
 import imgProgSony from "assets/img/prog-sony.png";
 import imgProgWfh from "assets/img/prog-wfh.png";
 import zedgeLogo from "assets/img/zedge-logo.png";
-import { CHAIN_TOKEN_SYMBOL, progInfoMeta } from "libs/config";
+import { BUTTONS, CHAIN_TOKEN_SYMBOL, progInfoMeta } from "libs/config";
 import { sleep } from "libs/utils/util";
 import { useChainMeta } from "store/ChainMetaContext";
+import ChainSupportedComponent from "components/UtilComps/ChainSupportedComponent";
 
 type MarshalFeatures = {
   [index: string]: any;
@@ -213,9 +214,11 @@ export default function AppMarketplace(props: Props) {
               <Button size="sm" mt="3" mr="3" colorScheme="teal" variant="outline" onClick={() => handleLearnMoreProg("gdc")}>
                 Learn More
               </Button>
-              <Button size="sm" mt="3" colorScheme="teal" onClick={() => handleJoinPS4Passport()}>
-                Join Now
-              </Button>
+              <ChainSupportedComponent feature={BUTTONS.JOIN_NOW}>
+                <Button size="sm" mt="3" colorScheme="teal" onClick={() => handleJoinPS4Passport()}>
+                  Join Now
+                </Button>
+              </ChainSupportedComponent>
             </Box>
           </Box>
 
