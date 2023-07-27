a22316bc6e715c0277172a2453d98bb1f2b50eba
Mark Paul <paul.mark@gmail.com>
2023-07-27 19:46:24 +1000

    fix issue with lightmode articles, remove unused files and images to reduce the pack size

--- src/components/Sections/ExplainerArticles.tsx
+++ src/components/Sections/ExplainerArticles.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading, Image, Text, Link, Card, CardBody, Stack, SimpleGrid } from "@chakra-ui/react";
+import { Box, Heading, Text, Link, Card, CardBody, Stack, SimpleGrid } from "@chakra-ui/react";
 import explainerArticleBG from "assets/img/explainer-article-bg.jpeg";
 
 const ExplainerArticles = ({ skipSpacing }: { skipSpacing?: boolean }) => {
@@ -48,7 +48,7 @@ function ArticleCard({ imgTitle, title, link }: { imgTitle: string; title: strin
                 backgroundRepeat="no-repeat"
                 h="150px"
                 w="auto"></Box>
-              <Text position="absolute" top="40px" left="25px" fontSize=".8rem" width="128px">
+              <Text position="absolute" top="40px" left="25px" fontSize=".8rem" width="128px" color="bgWhite">
                 {imgTitle}
               </Text>
             </Box>
