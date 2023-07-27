beb7cff0dfb9b4c68fdd0560417ac69656cc45d1
icegriffinguru <icegriffin1122@gmail.com>
2023-07-27 14:29:48 -0400

    [ice] remove nativeAuth

--- src/pages/App/AuthPickerMultiversX.tsx
+++ src/pages/App/AuthPickerMultiversX.tsx
@@ -79,7 +79,7 @@ function AuthPickerMx({ launchEnvironment, resetLaunchMode }: { launchEnvironmen
   const modelSize = useBreakpointValue({ base: "xs", md: "xl" });
 
   const commonProps = {
-    nativeAuth: true, // optional
+    // nativeAuth: true, // optional
     callbackRoute: pathname,
   };
 
