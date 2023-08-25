186b421b23db8f6f33a3151830e58eb8e0e5b94f
Enciusan <enciulescu.alexandru@gmail.com>
2023-08-25 16:11:44 +0300

    setup for wl page

---
+++ src/pages/GetWhitelist/components/TrendingData.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TrendingData: React.FC = () => {
+  return <></>;
+};
---
+++ src/pages/GetWhitelist/components/UseCases.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+export const UseCases: React.FC = () => {
+  return (
+    <Flex flexDirection="column" w="full" h="100dvh" justifyContent="center">
+      <Text textAlign="center" fontSize="4xl" fontFamily="Clash-Medium">
+        Data NFT Use Cases
+      </Text>
+    </Flex>
+  );
+};
