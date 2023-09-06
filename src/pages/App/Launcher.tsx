992d1bb357bffa7a5896127834334e4c73f11b7d
Damian <damian.ovidiu27@gmail.com>
2023-09-06 06:01:03 +0300

    hub hotfix

--- src/pages/App/Launcher.tsx
+++ src/pages/App/Launcher.tsx
@@ -9,29 +9,38 @@ import { walletConnectV2ProjectId, MX_TOAST_LIFETIME_IN_MS } from "libs/mxConsta
 import { clearAppSessionsLaunchMode } from "libs/utils";
 import MxAppHarness from "./AppHarnessMultiversX";
 import AuthPickerMx from "./AuthPickerMultiversX";
+import { useSearchParams } from "react-router-dom";
 
 function Launcher() {
+  const [searchParams] = useSearchParams();
   const [launchModeSession, setLaunchModeSession] = useLocalStorage("itm-launch-mode", null);
   const [launchEnvSession, setLaunchEnvSession] = useLocalStorage("itm-launch-env", null);
   const [launchMode, setLaunchMode] = useState(launchModeSession || "no-auth");
   const [hubAccessToken] = useSessionStorage("itm-hub-access-token", null);
-  const [launchEnvironment, setLaunchEnvironment] = useState(hubAccessToken ? "mainnet" : launchEnvSession || "devnet");
+  const [launchEnvironment, setLaunchEnvironment] = useState(hubAccessToken || searchParams.get("accessToken") ? "mainnet" : launchEnvSession || "devnet");
 
   // hoisting launchModeControl here allows us to go multi-chain easier in future
   // ... have a look at git history on this component
   const handleLaunchMode = (option: any, environment: any) => {
-    setLaunchMode(option);
-    setLaunchModeSession(option);
+    if (searchParams.get("accessToken")) {
+      setLaunchMode("mx");
+      setLaunchModeSession("mx");
+      setLaunchEnvironment("mainnet");
+      setLaunchEnvSession("mainnet");
+    } else {
+      setLaunchMode(option);
+      setLaunchModeSession(option);
 
-    if (environment) {
-      setLaunchEnvironment(environment);
-      setLaunchEnvSession(environment);
+      if (environment) {
+        setLaunchEnvironment(environment);
+        setLaunchEnvSession(environment);
+      }
     }
 
     // resetting all launch mode sessions here is nice an clean
     clearAppSessionsLaunchMode();
   };
-
+  console.log(launchEnvironment);
   return (
     <>
       <DappProvider
