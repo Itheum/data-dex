d0741c3c83ff4216215cfe91d8c714e9fec2a0ee
Enciusan <enciulescu.alexandru@gmail.com>
2023-11-23 20:44:03 +0100

    curatedNfts working

--- src/pages/Enterprise/components/DataNftCollection/CurateNfts.tsx
+++ src/pages/Enterprise/components/DataNftCollection/CurateNfts.tsx
@@ -1,47 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, Flex, Text, Image } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
 import { ContractConfiguration, DataNft, NftMinter } from "@itheum/sdk-mx-data-nft/out";
 import { useGetAccountInfo, useGetNetworkConfig } from "@multiversx/sdk-dapp/hooks";
 import { getApi } from "../../../../libs/MultiversX/api";
 import axios from "axios";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { NftType } from "@multiversx/sdk-dapp/types/tokens.types";
-import { getHealthCheckFromBackendApi } from "../../../../libs/MultiversX";
-import { useParams } from "react-router-dom";
-import { Address, IAddress } from "@multiversx/sdk-core/out";
+import { Address } from "@multiversx/sdk-core/out";
 import { sendTransactions } from "@multiversx/sdk-dapp/services";
+import { MdNavigateBefore, MdOutlineNavigateNext } from "react-icons/md";
 
 type CurateNftsProp = {
   nftMinter: NftMinter;
   viewContractConfig: ContractConfiguration;
 };
 
-type FreezeUnfreezeNfts = {
-  senderAddress: string;
-  nonce: number;
-  freezeAddress: string;
-  nftImgUrl: string;
-};
-type aaa = {
-  owner: string;
-} & DataNft;
-
 export const CurateNfts: React.FC<CurateNftsProp> = (props) => {
   const { nftMinter, viewContractConfig } = props;
   const [createDataNfts, setCreateDataNfts] = useState<Array<NftType>>([]);
   const [addressFrozenNonces, setAddressFrozenNonces] = useState<Array<number>>([]);
+  const [paginationFromNft, setPaginationFromNft] = useState<number>(0);
+  const [paginationSizeNft, setPaginationSizeNft] = useState<number>(10);
   const { chainID } = useGetNetworkConfig();
   const { address } = useGetAccountInfo();
-  const { minterAddress } = useParams();
   const tokenIdentifier = viewContractConfig.tokenIdentifier;
 
   DataNft.setNetworkConfig(chainID === "1" ? "mainnet" : "devnet");
   // console.log(createDataNfts);
-  const getCreatedDataNftsFromSdk = async () => {
+  const getCreatedDataNftsFromAPI = async () => {
     const apiLink = getApi(chainID);
-    const url = `https://${apiLink}/collections/${tokenIdentifier}/nfts?withOwner=true`;
+    const url = `https://${apiLink}/collections/${tokenIdentifier}/nfts?from=${paginationFromNft}&size=${paginationSizeNft}&withOwner=true`;
     const { data } = await axios.get(url);
     setCreateDataNfts(data);
+    // console.log(data);
   };
   // console.log(createDataNfts);
   const freezeDataNft = async (creator: string, nonce: number, owner: string | undefined) => {
@@ -67,14 +58,19 @@ export const CurateNfts: React.FC<CurateNftsProp> = (props) => {
   };
 
   useEffect(() => {
-    getCreatedDataNftsFromSdk();
+    getCreatedDataNftsFromAPI();
+
     // console.log(createDataNfts);
     (async () => {
       const viewAddressFrozenNonces = await nftMinter.viewAddressFrozenNonces(new Address(address));
-      console.log(viewAddressFrozenNonces);
+      // console.log(viewAddressFrozenNonces);
       setAddressFrozenNonces(viewAddressFrozenNonces);
     })();
   }, []);
+  useEffect(() => {
+    getCreatedDataNftsFromAPI();
+    // console.log(paginationFromNft);
+  }, [paginationFromNft]);
 
   // console.log(addressFrozenNonces);
   return (
@@ -84,8 +80,43 @@ export const CurateNfts: React.FC<CurateNftsProp> = (props) => {
       </Text>
       <Flex flexDirection="row" justifyItems="center" alignItems="center" gap={4}>
         <Text>You have {createDataNfts.length} Data NFT tokens in your collection</Text>
-        <Button variant="outline" color="teal.200" borderColor="teal.200" onClick={() => getCreatedDataNftsFromSdk()}>
+        <Button variant="outline" color="teal.200" borderColor="teal.200" onClick={() => getCreatedDataNftsFromAPI()}>
           Refresh
+        </Button>
+      </Flex>
+      <Flex justifyContent="center" pb={3} gap={3}>
+        <Button
+          colorScheme="teal"
+          alignItems="center"
+          justifyContent="center"
+          isDisabled={paginationFromNft <= 0}
+          onClick={() => {
+            if (paginationFromNft >= 0) {
+              setPaginationFromNft(paginationFromNft - paginationSizeNft);
+              getCreatedDataNftsFromAPI();
+            } else {
+              setPaginationFromNft(0);
+            }
+          }}>
+          <MdNavigateBefore />
+          Back
+        </Button>
+        <Button
+          colorScheme="teal"
+          alignItems="center"
+          justifyContent="center"
+          isDisabled={createDataNfts.length <= 10}
+          onClick={() => {
+            if (createDataNfts.length - paginationFromNft >= 10) {
+              setPaginationFromNft(paginationFromNft + paginationSizeNft);
+              getCreatedDataNftsFromAPI();
+              // console.log(paginationFromNft);
+            } else {
+              return;
+            }
+          }}>
+          Next
+          <MdOutlineNavigateNext />
         </Button>
       </Flex>
       <Flex flexDirection={{ base: "column", md: "row" }} flexWrap="wrap" justifyItems="start" alignItems="start" gap={3}>
@@ -104,8 +135,8 @@ export const CurateNfts: React.FC<CurateNftsProp> = (props) => {
                   <Flex flexDirection="row" gap={5}>
                     <Button
                       variant="outline"
-                      borderColor="teal"
-                      textColor="teal"
+                      borderColor="teal.200"
+                      textColor="teal.200"
                       fontSize="sm"
                       size="xs"
                       onClick={() => unFreezeDataNft(dataNfts.creator, dataNfts.nonce, dataNfts.owner)}>
@@ -121,7 +152,7 @@ export const CurateNfts: React.FC<CurateNftsProp> = (props) => {
                     borderColor="red"
                     textColor="red"
                     fontSize="sm"
-                    size="sm"
+                    size="xs"
                     onClick={() => freezeDataNft(dataNfts.creator, dataNfts.nonce, dataNfts.owner)}>
                     Freeze
                   </Button>
