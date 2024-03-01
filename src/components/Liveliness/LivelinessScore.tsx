14372945d3cc16bc9eee28a61a829b1bd0b9cc97
Enciusan <enciulescu.alexandru@gmail.com>
2024-03-01 13:36:56 +0200

    fix: changed hardcoded supply, changed the styles on details full page, changed the marketplace variable name

--- src/components/Liveliness/LivelinessScore.tsx
+++ src/components/Liveliness/LivelinessScore.tsx
@@ -3,14 +3,13 @@ import { Box, Flex, Progress, Text } from "@chakra-ui/react";
 import { settingLivelinessScore } from "../../libs/utils";
 
 type LivelinessScoreProp = {
-  index: number;
   tokenIdentifier?: string;
   unboundTimestamp?: number;
   lockPeriod?: number;
 };
-// not working properly
+
 export const LivelinessScore: React.FC<LivelinessScoreProp> = (props) => {
-  const { index, tokenIdentifier, unboundTimestamp, lockPeriod } = props;
+  const { tokenIdentifier, unboundTimestamp, lockPeriod } = props;
   const [livelinessScore, setLivelinessScore] = useState<number>(-1);
 
   useEffect(() => {
@@ -30,7 +29,7 @@ export const LivelinessScore: React.FC<LivelinessScoreProp> = (props) => {
           <Text fontSize="lg" fontWeight="light" display="flex" justifyContent="flex-start" pb="14px">
             Liveliness Score: {livelinessScore}
           </Text>
-          <Box border="2px solid" borderColor="teal.200" borderRadius="sm">
+          <Box border="1px solid" borderColor="teal.200" borderRadius="sm">
             <Progress hasStripe value={livelinessScore} rounded="xs" colorScheme="teal" />
           </Box>
         </Flex>
@@ -39,7 +38,7 @@ export const LivelinessScore: React.FC<LivelinessScoreProp> = (props) => {
           <Text fontSize="lg" fontWeight="light" display="flex" justifyContent="flex-start" pt={2} pb={1} textColor="indianred">
             Liveliness score expired.
           </Text>
-          <Box border="2px solid" borderColor="indianred" borderRadius="sm">
+          <Box border="1px solid" borderColor="indianred" borderRadius="sm">
             <Progress hasStripe value={livelinessScore} rounded="xs" colorScheme="red" />
           </Box>
         </Flex>
