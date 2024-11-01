d8663cf646bf3b22137dee30c5c76ddfad6086d7
Mark Paul <paul.mark@gmail.com>
2024-11-01 13:55:27 +1100

    feature: add mainnet solana data nft collection

--- src/libs/Solana/config.ts
+++ src/libs/Solana/config.ts
@@ -2,7 +2,7 @@ import { IS_DEVNET } from "libs/config";
 
 export const SUPPORTED_SOL_COLLECTIONS = IS_DEVNET
   ? ["6MgvQSDUU3Z2a5MQqPeStUyCo1AXrB8xJhyBc8YYH3uk", "6uXqBZLNdp3dT1wgJYHCXjCs36WWLThdQkKUeqdTT7ni", "68U3PakyLevtZ2A87iBS7JKuWeha2bcCfLdD8tY1SZ9u"]
-  : ["me2Sj97xewgEodSCRs31jFEyA1m3FQFzziqVXK9SVHX"];
+  : ["me2Sj97xewgEodSCRs31jFEyA1m3FQFzziqVXK9SVHX", "C6MwnegxKZfZjYbrGZ8DRBLUxvXohscn8xozNPzdkJNX"];
 
 export enum SolEnvEnum {
   devnet = "SD",
