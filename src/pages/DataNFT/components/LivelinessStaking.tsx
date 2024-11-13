3c2834d5a930409e2ab5941841939e4f4f145b97
Mark Paul <paul.mark@gmail.com>
2024-11-14 10:52:34 +1100

    chore: rebud xportal hub issues

--- src/pages/DataNFT/components/LivelinessStaking.tsx
+++ src/pages/DataNFT/components/LivelinessStaking.tsx
@@ -125,7 +125,7 @@ export const LivelinessStaking: React.FC = () => {
 
   async function handleClaimRewardsClick() {
     try {
-      alert("handle claim reward");
+      alert("handle claim reward 1");
 
       const envNetwork = import.meta.env.VITE_ENV_NETWORK;
       const liveContract = new LivelinessStake(envNetwork);
